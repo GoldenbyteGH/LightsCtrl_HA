bcff243fe9718007d5d2abf7e8504c5ac31a8c89
GoldenbyteGH <obertigiovannicesare@gmai.com>
2021-12-17 12:14:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import configparser,json,requests
+
+if __name__ == '__main__':
+
+    #get my lights entity_ids
+    lights_inv = json.loads(open("lights.json").read())
+
+    #get my token
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    #turn on all lights
+
+    for i in range (len(lights_inv)+1):
+        headers = {
+            "Authorization": "Bearer " + str(config['Account']['gb_token']),
+            "content-type": "application/json",
+            "Access - Control - Allow - Origin": "*"
+            }
+        data = {"entity_id":lights_inv["hlights"][i]["entity_id"]}
+        url = "http://homeassistant.goldenbyte.it:8123/api/services/switch/turn_on"
+        data=json.dumps(data)
+        responce = requests.post(url,data=data,headers=headers)
+        print(responce)
