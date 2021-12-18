f9f11ef2632677006ba1d879b6be16d4372a2ea8
GoldenbyteGH <48603774+GoldenbyteGH@users.noreply.github.com>
2021-12-18 01:46:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+#!/usr/bin/python3
 """
 Script to turn on all lights specified into 'lights.json' file
-
 """
 
-import configparser,json,requests
+import sys
+import json,requests
+import configparser
+
 
 
 if __name__ == '__main__':
+
+    #default state is OFF
+    state = 'off'
+    try:
+        arg = sys.argv[1]
+        arg=arg.lower()
+        if 'on' == arg:state ='on'
+    except:
+        print("ERROR - missing on/off state parameter")
+        exit()
 
     #get my lights entity_ids
     lights_inv = json.loads(open("lights.json").read())
@@ -24,7 +37,7 @@ if __name__ == '__main__':
             "Access - Control - Allow - Origin": "*"
             }
         data = {"entity_id":lights_inv["hlights"][i]["entity_id"]}
-        url = "http://homeassistant.goldenbyte.it:8123/api/services/light/turn_on"
+        url = "http://homeassistant.goldenbyte.it:8123/api/services/light/turn_"+state
         data=json.dumps(data)
         responce = requests.post(url,data=data,headers=headers)
         print(responce)
